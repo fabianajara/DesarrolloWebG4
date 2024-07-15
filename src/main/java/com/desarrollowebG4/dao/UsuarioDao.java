30b5948bfbce5c4b22ee4d623c02473e4beadda8
fabianajara <fabianajara.u@gmail.com>
2024-07-15 14:50:13 -0600

    Parte lógica de Usuario
    CRUD de la parte usuarios (no incluye parte grafica)

--- src/main/java/com/desarrollowebG4/dao/UsuarioDao.java
+++ src/main/java/com/desarrollowebG4/dao/UsuarioDao.java
@@ -5,4 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {
 
+    Usuario findByEmail(String email);
+
+    Usuario findByTelefono(String telefono);
+
+    Usuario findByEmailAndContrasena(String email, String contrasena);
+
+    Usuario findByTelefonoAndContrasena(String telefono, String contrasena);
+
+    Usuario findByEmailOrTelefono(String email, String telefono);
+
+    boolean existsByEmailOrTelefono(String email, String telefono);
+
+    Usuario findByIdUsuario(Long idUsuario);
 }
