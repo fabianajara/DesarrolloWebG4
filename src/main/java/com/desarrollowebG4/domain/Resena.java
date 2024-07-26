814171a54f8ab0b453e26891afbb7fe1a8cdcf46
MelCR29 <85038046+MelCR29@users.noreply.github.com>
2024-07-25 18:22:20 -0600

    Domain

---
+++ src/main/java/com/desarrollowebG4/domain/Resena.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.desarrollowebG4.domain;
+
+// especificar que es una capa de datos
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "Resena", schema = "homeaway")
+public class Resena implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_resena")
+    private Long idResena;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_alojamiento", nullable = false)
+    private Alojamiento alojamiento;
+
+    @Column(name = "calificacion")
+    private double calificacion;
+
+    @Column(name = "comentario")
+    private String comentario;
+
+    @Column(name = "fecha")
+    private Date fecha;
+
+    // Getters and Setters
+
+    public Long getIdResena() {
+        return idResena;
+    }
+
+    public void setIdResena(Long idResena) {
+        this.idResena = idResena;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Alojamiento getAlojamiento() {
+        return alojamiento;
+    }
+
+    public void setAlojamiento(Alojamiento alojamiento) {
+        this.alojamiento = alojamiento;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
