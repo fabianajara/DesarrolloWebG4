e74df7bdab75e10c06fb10fbb63239ade1d80643
fabianajara <fabianajara.u@gmail.com>
2024-07-25 22:16:47 -0600

    Revert "Alojamientos USUARIOS"
    This reverts commit bd38c70142afdf58f382275b3675b8e3da39231c.

--- src/main/java/com/desarrollowebG4/controller/UsuarioAlojamientoController.java
+++ src/main/java/com/desarrollowebG4/controller/UsuarioAlojamientoController.java
@@ -13,20 +13,17 @@ public class UsuarioAlojamientoController {
     @Autowired
     private AlojamientoService alojamientoService;
 
-    // Se encarga de lleavr a la lista de todos los alojamientos
     @GetMapping("/usuario/alojamientos")
     public String listarAlojamientos(Model model) {
         model.addAttribute("alojamientos", alojamientoService.listarAlojamientos());
-        return "usuario/alojamientos/listado"; 
+        return "usuario/alojamientos/listado"; // Asegúrate de que la plantilla se llama listado.html
     }
 
-    // Se encarga de llevar a los detalles de un alojamiento especifico
     @GetMapping("/usuario/alojamientos/{id}")
     public String verAlojamiento(@PathVariable Long id, Model model) {
         alojamientoService.buscarAlojamientoPorId(id).ifPresent(alojamiento -> {
             model.addAttribute("alojamiento", alojamiento);
         });
-        return "usuario/alojamientos/detalle"; 
+        return "usuario/alojamientos/detalle"; // Verifica que la plantilla detalle.html exista
     }
-
 }
